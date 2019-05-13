2afe9c64f9b2b96809b070d8578960699de46f21
sabbir-rupom <sabbir.rupom@gmail.com>
2019-05-13 10:20:56 +0600

    Source file initialized
    Added helper class
    Added composer file for sdk inslattion
    Added config.ini for sdk setup configuration

---
+++ AwsS3Helper.php
@@ -0,0 +1,351 @@
+<?php
+
+// Require the Composer autoloader.
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+class AwsS3Helper {
+
+    // Define member variables
+    private $s3Client;
+    protected $config;
+    protected $result;
+    // Hold an instance of the class
+    private static $instance;
+
+    /**
+     * Initiate helper class constructor
+     */
+    private function __construct() {
+        /*
+         * Initialize member variables
+         */
+        $this->s3Client = new S3Client($this->_getConfig());
+        $this->result = array(
+            'success' => FALSE,
+            'msg' => ''
+        );
+    }
+
+    /**
+     * Get AWS SDK configuration settings
+     * @return array [] Retrieve SDK configuration array
+     *      [version]       SDK version
+     *      [region]        Used AWS Region
+     *      [credentials]   AWS Authentication Token Array
+     *          [key]       AWS Access Token
+     *          [secret]    AWS Secret Access Key
+     */
+    private function _getConfig() {
+
+        if (!file_exists(__DIR__ . '/config.ini')) {
+            die("SDK configuration failed. 'config.ini' not found.");
+        }
+
+        $this->config = parse_ini_file(__DIR__ . '/config.ini');
+        
+        if (empty($this->config['AWS_REGION']) || empty($this->config['ACCESS_TOKEN']) || empty($this->config['SECRET_KEY'])) {
+            die("Configuration failed. Settings data is empty - please check 'config.ini' file");
+        }
+
+        // Initiate config parameters for AWS SDK library 
+        return array(
+            'version' => 'latest',
+            'region' => $this->config['AWS_REGION'],
+            'credentials' => [
+                'key' => $this->config['ACCESS_TOKEN'],
+                'secret' => $this->config['SECRET_KEY']
+            ]
+        );
+    }
+
+    /**
+     * The singleton method
+     * 
+     * @return obj single class instance
+     */
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Get S3 connection object
+     * 
+     * @return obj AWS S3 Client
+     */
+    public function getS3Client() {
+        return $this->s3Client;
+    }
+
+    /**
+     * Upload file in AWS S3 storage server
+     * 
+     * @param string $bucketName Name of S3 bucket
+     * @param string $filekey full pathname of AWS file 
+     * @param string $fileLocation File source location in local directory
+     * @param string $contentType Type of file-content
+     * @param bool $public Set public status true/false for uploaded file
+     * @return array [] Result array
+     *      [fileUrl]       string Live url of uploaded object, if uploaded successfully
+     *      [success]       bool Success result
+     *      [msg]           string Exception message if occurred during function execution
+     */
+    public function uploadFileToS3($bucketName, $filekey, $fileLocation, $contentType, $public = true) {
+        try {
+            $configArray = [
+                'Bucket' => $bucketName,
+                'Key' => $filekey,
+                'SourceFile' => $fileLocation,
+                'ACL' => $public ? 'public-read' : 'authenticated-read',
+                'StorageClass' => 'REDUCED_REDUNDANCY'
+            ];
+            if (!empty($contentType)) {
+                $configArray['ContentType'] = $contentType;
+            }
+
+            $result = $this->s3Client->putObject($configArray);
+            $this->result['fileUrl'] = $this->s3Client->getObjectUrl($bucketName, $filekey);
+            $this->result['success'] = TRUE;
+        } catch (AwsException $e) {
+            // output error message if fails
+            $this->result['msg'] = $e->getMessage();
+            $this->result['success'] = FALSE;
+        }
+
+        return $this->result;
+    }
+
+    /**
+     * Check object file exist or not
+     * 
+     * @param string $bucketName
+     * @param string $fileKey
+     * @return bool Return TRUE if object/file exist, otherwise FALSE
+     */
+    public function checkFileExistInS3($bucketName, $fileKey) {
+        return $this->s3Client->doesObjectExist($bucketName, $fileKey);
+    }
+
+    /**
+     * Get base URL path of objects in S3 bucket 
+     * 
+     * @param string $bucketName
+     * @return string Base URL of s3 object in bucket
+     */
+    public function getS3UrlPath($bucketName) {
+        return 'https://' . $bucketName . '.s3.' . $this->config['AWS_REGION'] . '.amazonaws.com/';
+    }
+
+    /**
+     * Delete specific file from AWS S3 storage server
+     * 
+     * @param string $bucketName Name of the S3 bucket
+     * @param string $fileKey    S3 file path to object
+     * @return array [] Result array
+     *      [success]       bool Success result
+     *      [msg]           string Exception message if occurred during function execution
+     */
+    public function deleteFileFromS3($bucketName, $fileKey) {
+        try {
+            if ($this->checkFileExistInS3($bucketName, $fileKey)) {
+                $result = $this->s3Client->deleteObject(array(
+                    'Bucket' => $bucketName,
+                    'Key' => $fileKey,
+                ));
+            }
+            $this->result['success'] = TRUE;
+        } catch (AwsException $e) {
+            // output error message if fails
+            $this->result['msg'] = $e->getMessage();
+            $this->result['success'] = FALSE;
+        }
+
+        return $this->result;
+    }
+
+    /**
+     * Delete all files from specified path of AWS S3 storage server
+     * 
+     * @param string $bucketName Name of the S3 bucket
+     * @param string $dirPrefix Directory path under which files needed to be checked
+     * @return array [] Result array
+     *      [success]       bool Success result
+     *      [msg]           string Exception message if occurred during function execution
+     */
+    public function deleteDirectoryFilesFromS3($bucketName, $dirPrefix) {
+        try {
+            $results = $this->s3Client->listObjectsV2([
+                'Bucket' => $bucketName,
+                'Prefix' => $dirPrefix
+            ]);
+
+            if (isset($results['Contents'])) {
+                foreach ($results['Contents'] as $result) {
+                    $this->s3Client->deleteObject([
+                        'Bucket' => $bucketName,
+                        'Key' => $result['Key']
+                    ]);
+                }
+            }
+
+            $this->result['success'] = TRUE;
+        } catch (AwsException $e) {
+            // output error message if fails
+            $this->result['msg'] = $e->getMessage();
+            $this->result['success'] = FALSE;
+        }
+
+        return $this->result;
+    }
+
+    /**
+     * Get all bucket list in AWS S3
+     * @return array [] Result array
+     *      [success]       bool Success result
+     *      [msg]           string Exception message if occurred during function execution
+     */
+    public function getBucketList() {
+        try {
+            $result = $this->s3Client->listBuckets();
+            $this->result['bucketList'] = $result->toArray();
+            $this->result['success'] = TRUE;
+        } catch (AwsException $e) {
+            // output error message if fails
+            $this->result['msg'] = $e->getMessage();
+            $this->result['success'] = FALSE;
+        }
+
+        return $this->result;
+    }
+
+    /**
+     * Create new bucket in AWS S3
+     * 
+     * @param string $name Name of the bucket
+     * @param bool $public Public read status in live
+     * @return array [] Result array
+     *      [success]       bool Success result
+     *      [msg]           string Exception message if occurred during function execution
+     */
+    public function createBucket($name, $public = true) {
+        try {
+            $result = $this->s3Client->createBucket([
+                'Bucket' => $bucketName, // REQUIRED
+                'ACL' => $public ? 'public-read' : 'authenticated-read',
+            ]);
+            $this->result['success'] = TRUE;
+        } catch (AwsException $e) {
+            // output error message if fails
+            $this->result['msg'] = $e->getMessage();
+            $this->result['success'] = FALSE;
+        }
+
+        return $this->result;
+    }
+
+    /**
+     * Retrieve file content type from filename
+     * 
+     * @param string $filename Name of file with extension
+     * @return string File Mime Type [ if not matched default mime-type returned 'application/octet-stream' ]
+     */
+    public static function getMimeType($filename) {
+        $idx1 = explode('.', $filename);
+        $count_explode = count($idx1);
+        $idx = strtolower($idx1[$count_explode - 1]);
+
+        $mimet = array(
+            'txt' => 'text/plain',
+            'csv' => 'text/csv',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+            // Images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+            // Archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+            // Audio/video
+            'mpg' => 'audio/mpeg',
+            'mp2' => 'audio/mpeg',
+            'mp3' => 'audio/mpeg',
+            'mp4' => 'audio/mp4',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+            'ogg' => 'audio/ogg',
+            'oga' => 'audio/ogg',
+            'wav' => 'audio/wav',
+            'webm' => 'audio/webm',
+            'aac' => 'audio/aac',
+            // Adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+            // MS Office
+            'doc' => 'application/msword',
+            'dot' => 'application/msword',
+            'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'dotx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+            'docm' => 'application/vnd.ms-word.document.macroEnabled.12',
+            'dotm' => 'application/vnd.ms-word.template.macroEnabled.12',
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'xlt' => 'application/vnd.ms-excel',
+            'xla' => 'application/vnd.ms-excel',
+            'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'xltx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+            'xlsm' => 'application/vnd.ms-excel.sheet.macroEnabled.12',
+            'xltm' => 'application/vnd.ms-excel.template.macroEnabled.12',
+            'xlam' => 'application/vnd.ms-excel.addin.macroEnabled.12',
+            'xlsb' => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+            'ppt' => 'application/vnd.ms-powerpoint',
+            'pot' => 'application/vnd.ms-powerpoint',
+            'pps' => 'application/vnd.ms-powerpoint',
+            'ppa' => 'application/vnd.ms-powerpoint',
+            'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+            'potx' => 'application/vnd.openxmlformats-officedocument.presentationml.template',
+            'ppsx' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+            'ppam' => 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+            'pptm' => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+            'potm' => 'application/vnd.ms-powerpoint.template.macroEnabled.12',
+            'ppsm' => 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+            'mdb' => 'application/vnd.ms-access',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet'
+        );
+
+        if (isset($mimet[$idx])) {
+            return $mimet[$idx];
+        } else {
+            return 'application/octet-stream';
+        }
+    }
+
+}
